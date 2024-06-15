caa16f08c33b54992aa99aa9a307ec6ec1034e98
ashwinrah <raikr5865@gmail.com>
2024-06-15 19:43:18 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const fileInput = document.getElementById('file');
-
-fileInput.addEventListener('change', (e) => {
-  const file = fileInput.files[0];
-  if (!file.type.match('image/jpeg')) {
-    alert('Only JPG files are allowed');
-    fileInput.value = '';
-  }
-});
